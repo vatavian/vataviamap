41b2cc1dae08bb79268f442220fb134088a1d4d7
mark.gray <mark.gray@4228ad40-4302-11de-bba7-a9cb5bbe15e6>
2010-02-11 20:44:57 +0000

    Shared/clsLayer.LoadDMTP: attempt to set time local vs UTC correctly        modGlobal: mutex to protect log file from simultaneous writes on two threads Desktop: frmEditNameURL.Ok event split into Add and Change

--- VataviaMap/Desktop/frmEditNameURL.vb
+++ VataviaMap/Desktop/frmEditNameURL.vb
@@ -1,15 +1,11 @@
 Public Class frmEditNameURL
 
-    ''' <summary>
-    ''' Event is raised when user presses Ok
-    ''' </summary>
-    ''' <param name="aName">Name currently displayed</param>
-    ''' <param name="aURL">URL to get buddy location</param>
-    ''' <remarks></remarks>
-    Public Event Ok(ByVal aOriginalName As String, ByVal aName As String, ByVal aURL As String)
+    Public Event Add(ByVal aName As String, ByVal aURL As String)
+    Public Event Change(ByVal aOriginalName As String, ByVal aName As String, ByVal aURL As String)
     Public Event Remove(ByVal aOriginalName As String)
 
     Private pOrigName As String
+    Private pEdit As Boolean = False
 
     ''' <summary>
     ''' Ask the user for a Name and URL
@@ -18,12 +14,13 @@ Public Class frmEditNameURL
     ''' <param name="aName">Starting value for Name text box</param>
     ''' <param name="aURL">Starting value for URL text box</param>
     ''' <remarks></remarks>
-    Public Sub AskUser(ByVal aTitle As String, ByVal aName As String, ByVal aURL As String, _
+    Public Sub AskUser(ByVal aTitle As String, ByVal aName As String, ByVal aURL As String, ByVal aEdit As Boolean, _
                        ByVal aLabelExamples As String, ByVal aButtonExamples As String)
         pOrigName = aName
         Me.Text = aTitle
         txtName.Text = aName
         txtURL.Text = aURL
+        pEdit = aEdit
         lblExamples.Text = aLabelExamples
         btnExamples.Tag = aButtonExamples
         Me.Show()
@@ -41,12 +38,16 @@ Public Class frmEditNameURL
     ''' </summary>
     Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
         Me.Hide()
-        RaiseEvent Ok(pOrigName, txtName.Text, txtURL.Text)
+        If pEdit Then
+            RaiseEvent Change(pOrigName, txtName.Text, txtURL.Text)
+        Else
+            RaiseEvent Add(txtName.Text, txtURL.Text)
+        End If
         Me.Close()
     End Sub
 
     ''' <summary>
-    ''' User pressed Remove, send Ok event with blank URL
+    ''' User pressed Remove
     ''' </summary>
     Private Sub btnRemove_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRemove.Click
         Me.Hide()
